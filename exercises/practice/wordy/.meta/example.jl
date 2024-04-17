2b7cb54bbb746791745396d27c1ddfbbd4377243
depial <91621102+depial@users.noreply.github.com>
2024-04-17 16:24:40 -0400

    add practice exercise wordy

---
+++ exercises/practice/wordy/.meta/example.jl
@@ -0,0 +1,11 @@
+function readeval(eq)
+    m = match(r"-?\d+\.?\d* [-+*/] -?\d+\.?\d*", eq)
+    isnothing(m) ? match(r"^-?\d+\.?\d*$", eq) : readeval(replace(eq, m.match => eval(Meta.parse(m.match))))
+end
+
+function wordy(problem)
+    optuples = zip(("plus","minus","divided by","multiplied by","What is ","?"), ("+","-","/","*","",""))
+    foreach(op -> problem = replace(problem, first(op) => last(op)), optuples)
+    answer = readeval(problem)
+    isnothing(answer) ? throw(ArgumentError(problem)) : eval(Meta.parse(answer.match))
+end
